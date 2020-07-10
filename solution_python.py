ab90b811494e36557ec817190fa7d2f775460532
Sahil Sangani <sahilsangani2@gmail.com>
2020-07-10 14:55:49 -0400

    Update solution_python.py

--- solution_python.py
+++ solution_python.py
@@ -1,23 +1,51 @@
 class EventSourcer():
     # Do not change the signature of any functions
+    tracker = []
+    delete = []
 
     def __init__(self):
         self.value = 0
+        EventSourcer.tracker.append(self.value)
 
     def add(self, num: int):
-        pass
+        ans = EventSourcer.tracker[-1] + num
+        EventSourcer.tracker.append(ans)
+        self.value = ans
+        print(ans)
+        return ans
 
     def subtract(self, num: int):
-        pass
+        ans = EventSourcer.tracker[-1] - num
+        EventSourcer.tracker.append(ans)
+        self.value = ans
+        return ans
 
     def undo(self):
-        pass
+        # self.value = EventSourcer.tracker.append(self.value[-1])
+        EventSourcer.delete.append(EventSourcer.tracker[-1])
+        del EventSourcer.tracker[-1]
+        ans = EventSourcer.tracker[-1]
+        self.value = ans
+        return ans
 
     def redo(self):
-        pass
+        # self.value = EventSourcer.tracker.append(self.value[-2])
+        EventSourcer.tracker.append(EventSourcer.delete[-1])
+        del EventSourcer.delete[-1]
+        ans = EventSourcer.tracker[-1]
+        self.value = ans
+        return ans
 
     def bulk_undo(self, steps: int):
-        pass
+        EventSourcer.delete.append(EventSourcer.tracker[-steps])
+        del EventSourcer.tracker[-steps]
+        ans = EventSourcer.tracker[-1]
+        self.value = ans
+        return ans
 
     def bulk_redo(self, steps: int):
-        pass
+        EventSourcer.tracker.append(EventSourcer.delete[-steps])
+        del EventSourcer.delete[-steps]
+        ans = EventSourcer.tracker[-1]
+        self.value = ans
+        return ans
